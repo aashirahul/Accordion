92d22dac386b256e7337c91e726375c8208b5a59
Ashima Shrivastava <ashima206@gmail.com>
2016-10-13 13:39:03 -0400

    MAde some slight changes in my js & scss file

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 var toggleAccordion = function(event){
 	$(".section").removeClass("open");
+	$(".section").addClass("close");
 	var target = event.target;
 	var parent = $(target).parent();
+	parent.removeClass("close");
 	parent.addClass("open");
 	
 }
